5e508de2e5671c3dbf142ac24f6f883868e548e7
mindkeeper <fcb.nyak@gmail.com>
2023-08-03 21:51:13 +0700

    creating store for redux and persistor

---
+++ src/redux/store.js
@@ -0,0 +1,31 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import authSlice from "./reducers/authSlice";
+import { authQuery } from "./reducers/authQuery";
+import { productQuery } from "./reducers/productQuery";
+import persistStore from "redux-persist/es/persistStore";
+import { persistReducer } from "redux-persist";
+import { categoryQuery } from "./reducers/categoryQuery";
+const persistConfig = {
+  storage,
+  key: "root",
+};
+
+const reducer = combineReducers({
+  auth: authSlice,
+  [authQuery.reducerPath]: authQuery.reducer,
+  [productQuery.reducerPath]: productQuery.reducer,
+  [categoryQuery.reducerPath]: categoryQuery.reducer,
+});
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false })
+      .concat(authQuery.middleware)
+      .concat(productQuery.middleware)
+      .concat(categoryQuery.middleware),
+});
+
+export const persistor = persistStore(store);
