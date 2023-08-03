c9b7f2afe6ac79b2e6e90451a9afb24a0937c5fa
mindkeeper <fcb.nyak@gmail.com>
2023-08-03 21:52:57 +0700

    impelementing redux and main layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { PersistGate } from "redux-persist/integration/react";
+import MainLayout from "./components/Layout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Product from "./pages/Product/Product";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <MainLayout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/products" element={<Product />} />
+            </Routes>
+          </MainLayout>
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
